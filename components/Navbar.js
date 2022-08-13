961d4523f1be6938e64c69565cf02f4933a96c52
Your Name <you@example.com>
2022-08-13 19:23:50 +0800

    logo and animations added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,19 @@
 import styles from "../styles/Navbar.module.scss";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { GiSharpShuriken } from "react-icons/gi";
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
-      <motion.div
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        className={styles.navbar_logo}
-      >
+      <div className={styles.navbar_logo}>
+        <img
+          className={styles.navbar_shuriken}
+          src="images/logo.svg"
+          alt="Anime Binge Logo"
+        />
         <Link href="/">Anime Binge</Link>
-      </motion.div>
+      </div>
       <div className={styles.navbar_links_container}>
         <Link href="/">
           <a className={styles.navbar_links}>Home</a>
